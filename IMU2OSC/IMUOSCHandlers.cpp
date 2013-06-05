2344995f1f3178201fb781996248b82744ac0975
Jamie Bullock <jamie@jamiebullock.com>
2013-06-05 21:15:09 +0100

    Dynamically allocate UDP transmit socket

--- IMU2OSC/IMUOSCHandlers.cpp
+++ IMU2OSC/IMUOSCHandlers.cpp
@@ -17,17 +17,17 @@
 #define ADDRESS "127.0.0.1"
 #define PORT 7000
 
-UdpTransmitSocket transmitSocket(IpEndpointName(ADDRESS, PORT));
-
-//void osc_init(const char *address, unsigned int port)
-//{
-//    IpEndpointName endpointName(
-//}
+UdpTransmitSocket *transmitSocket = NULL;
 
 void osc_init(const char *address, unsigned int port)
 {
-    UdpTransmitSocket newTransmitSocket(IpEndpointName(address, port));
-    transmitSocket = newTransmitSocket;
+//    UdpTransmitSocket newTransmitSocket(IpEndpointName(address, port));
+    if (transmitSocket != NULL)
+    {
+        delete transmitSocket;
+    }
+    
+    transmitSocket = new UdpTransmitSocket(IpEndpointName(address, port));
 }
 
 void osc_send_euler(double *euler) {
@@ -36,7 +36,15 @@ void osc_send_euler(double *euler) {
     osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
     
     p << osc::BeginMessage( "/x-imu/euler" ) << (float)euler[0] << (float)euler[1] << (float)euler[2] << osc::EndMessage;
-    transmitSocket.Send( p.Data(), p.Size() );
+
+    if (transmitSocket->IsConnected())
+    {
+        transmitSocket->Send( p.Data(), p.Size() );
+    }
+    else
+    {
+        printf("No OSC connection\n");
+    }
     p.Clear();
 
 }
@@ -47,7 +55,11 @@ void osc_send_cal(double *gyro, double *accel, double *mag) {
     osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
     
     p << osc::BeginMessage( "/x-imu/calibrated" ) << (float)gyro[0] << (float)gyro[1] << (float)gyro[2] << (float)accel[0] << (float)accel[1] << (float)accel[2] << (float)mag[0] << (float)mag[1] << (float)mag[2] << osc::EndMessage;
-    transmitSocket.Send( p.Data(), p.Size() );
+    
+    if (transmitSocket->IsConnected())
+    {
+        transmitSocket->Send( p.Data(), p.Size() );
+    }
     p.Clear();
     
 }
