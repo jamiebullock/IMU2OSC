6e6e3b87d80e47bf5d902872a489bd2c05593e1c
Jamie Bullock <jamie@jamiebullock.com>
2013-06-18 14:35:18 +0100

    Change copyright to BCU

--- IMU2OSC/IMUOSCHandlers.h
+++ IMU2OSC/IMUOSCHandlers.h
@@ -3,7 +3,7 @@
 //  IMU2OSC
 //
 //  Created by Jamie Bullock on 18/05/2012.
-//  Copyright (c) 2012 Jamie Bullock. All rights reserved.
+//  Copyright (c) 2012 Birmingham City University. All rights reserved.
 //
 
 #ifndef IMU2OSC_IMUOSCHandlers_h
